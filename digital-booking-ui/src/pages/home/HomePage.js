25263ed786a066f21ec89d3c6d4bb120c04e0deb
AdrielIg <adrielgomezignacio@gmail.com>
2022-05-25 13:03:34 -0300

    add search section with cities input

--- digital-booking-ui/src/pages/home/HomePage.js
+++ digital-booking-ui/src/pages/home/HomePage.js
@@ -1,11 +1,13 @@
 import { makeStyles } from "@material-ui/core";
 import React from "react";
 import Categories from "../../components/home/Categories";
+import SearchSection from "../../components/home/SearchSection";
 
 const HomePage = () => {
   const classes = useStyles();
   return (
     <main className={classes.body}>
+      <SearchSection />
       <Categories />
     </main>
   );
