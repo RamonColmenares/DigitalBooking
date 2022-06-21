01fc24c82038ee7da96a888a9dc1b10891b1fb75
NFerrovia <nicolasferrovia@gmail.com>
2022-06-21 20:33:35 -0300

    fix typo fetch

--- digital-booking-ui/src/client/referenceData/fetchCities.js
+++ digital-booking-ui/src/client/referenceData/fetchCities.js
@@ -1,7 +1,7 @@
 export const fetchCities = async () => {
   try {
     const response = await fetch(
-      'http://my-alb-1936629616.us-east-1.elb.amazonaws.com/cities/'
+      'http://my-alb-1936629616.us-east-1.elb.amazonaws.com/cities'
     );
     const data = await response.json();
     return data;
