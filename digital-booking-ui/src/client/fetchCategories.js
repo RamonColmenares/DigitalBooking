cfce15d9b49afc6cdf98fbea7d62fb34fbd7c5d1
NFerrovia <nicolasferrovia@gmail.com>
2022-06-21 20:19:03 -0300

    fix fetches

--- digital-booking-ui/src/client/fetchCategories.js
+++ digital-booking-ui/src/client/fetchCategories.js
@@ -1,7 +1,7 @@
 export const fetchCategories = async () => {
   try {
     const response = await fetch(
-      'http://my-alb-1936629616.us-east-1.elb.amazonaws.com/products/'
+      'http://my-alb-1936629616.us-east-1.elb.amazonaws.com/categories'
     );
     const data = await response.json();
     return data;
