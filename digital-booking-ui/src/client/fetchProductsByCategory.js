69ef7e436a17a18d72cf3b75a03cb5e6832f51cb
NFerrovia <nicolasferrovia@gmail.com>
2022-06-21 21:33:50 -0300

    fetch typo

--- digital-booking-ui/src/client/fetchProductsByCategory.js
+++ digital-booking-ui/src/client/fetchProductsByCategory.js
@@ -1,7 +1,7 @@
 export const fetchProductsByCategory = async (id) => {
   try {
     const response = await fetch(
-      `http://my-alb-1936629616.us-east-1.elb.amazonaws.com/products/${id}`
+      `http://my-alb-1936629616.us-east-1.elb.amazonaws.com/products/allCategory/${id}`
     );
     const data = await response.json();
     return data;
