232b11e3dab147b14419fc424a3369f0c6f6389e
Ramon Colmenares <ramon.colmenaresblanco@gmail.com>
2022-06-23 10:09:42 -0500

    fix bug

--- Back-End/piG1/src/main/java/com/example/piG1/Repository/IBookingRepository.java
+++ Back-End/piG1/src/main/java/com/example/piG1/Repository/IBookingRepository.java
@@ -22,10 +22,10 @@ public interface IBookingRepository extends JpaRepository<Booking,Integer> {
     List<Booking> findByProductId(Integer productId);
 
     @Query("select b from Booking b where " +
-            "(start_date between :startDate AND :endDate) " +
+            "((start_date between :startDate AND :endDate) " +
             "OR (end_date between :startDate AND :endDate) " +
             "OR ((:endDate between start_date AND end_date) AND (:startDate between start_date AND end_date)) "+
-            "AND product_id = :productId")
+            ") AND product_id = :productId")
     List<Booking> findByDatesBetweenAndProductId(
             @Param("startDate") LocalDate startDate,
             @Param("endDate") LocalDate endDate,
