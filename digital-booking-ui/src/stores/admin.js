5a99823cefde23cab25a1c411dc89abf3edb9695
AdrielIg <adrielgomezignacio@gmail.com>
2022-06-26 20:10:29 -0300

    Add admin store

---
+++ digital-booking-ui/src/stores/admin.js
@@ -0,0 +1,36 @@
+import { create } from "../utils/createStore";
+
+const INITIAL_STATE = {
+  name: "",
+  category: "",
+  city: {},
+  address: "",
+  description: "",
+  latitude: "",
+  longitude: "",
+};
+
+const createAdminStore = () =>
+  create("accommodation")((set, get) => ({
+    ...INITIAL_STATE,
+    loading: false,
+    loaded: false,
+    error: false,
+
+    setName: (name) => set({ name }),
+    setCategory: (category) => set({ category }),
+    setAddress: (address) => set({ address }),
+    setDescription: (description) => set({ description }),
+    setLatitude: (latitude) => set({ latitude }),
+    setLongitude: (longitude) => set({ longitude }),
+    setCity: (city) => {
+      set({
+        city: {
+          name: "Buenos Aires",
+          name_country: "Argentina",
+        },
+      });
+    },
+  }));
+
+export const useAdminStore = createAdminStore();
