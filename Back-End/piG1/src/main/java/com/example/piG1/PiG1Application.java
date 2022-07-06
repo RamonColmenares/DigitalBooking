cd8f6338130dec14f5ddc220059928ec71a00423
Ramon Colmenares <ramon.colmenaresblanco@gmail.com>
2022-07-06 17:40:43 -0500

    remove creation of roles from the main application

--- Back-End/piG1/src/main/java/com/example/piG1/PiG1Application.java
+++ Back-End/piG1/src/main/java/com/example/piG1/PiG1Application.java
@@ -1,34 +1,12 @@
 package com.example.piG1;
 
-import com.example.piG1.Model.Entity.Role;
-import com.example.piG1.Service.UserServices;
-import com.example.piG1.Service.RoleServices;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class PiG1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PiG1Application.class, args);
-	}
-
-	@Bean
-	PasswordEncoder passwordEncoder(){
-		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	CommandLineRunner run(UserServices userServices, RoleServices roleServices) {
-		return args -> {
-			if (roleServices.findRoleByName("ADMIN") == null || roleServices.findRoleByName("CLIENT") == null){
-			userServices.saveRole(new Role(null,"ADMIN"));
-			userServices.saveRole(new Role(null,"CLIENT"));
-			}
-		};
 	}
 }
