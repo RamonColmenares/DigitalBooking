7ba585b1aade237d5e4a66d54bb77dbd3374ecf7
Ramon Colmenares <ramon.colmenaresblanco@gmail.com>
2022-07-06 13:13:43 -0500

    Cors fix test

--- Back-End/piG1/src/main/java/com/example/piG1/Security/SecurityConfig.java
+++ Back-End/piG1/src/main/java/com/example/piG1/Security/SecurityConfig.java
@@ -16,15 +16,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
